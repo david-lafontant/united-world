20c93dd62dc62bf09696875f4382c43720d7de18
David Lafontant <lafontant.david@gmail.com>
2024-03-13 18:59:24 -0400

    Set row that display social medis, my page , and login link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from './Header.module.css';
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+const Header = () => (
+  <header className="container-fluid">
+    <div className={`row ${styles.login_social}`}>
+      <div className="col">
+        <ul className={`${styles.login_social_list}`}>
+          <li>Twitter</li>
+          <li>Facebook</li>
+          <li>English</li>
+          <li>My Page</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </div>
+  </header>
+);
 
 export default Header;
