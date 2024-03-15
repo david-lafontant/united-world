bcc32c0d783c4b092daa83c7325059d9e0c55095
David Lafontant <lafontant.david@gmail.com>
2024-03-15 04:12:37 -0400

    Create Home page and related css module file

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+      ad quos inventore aperiam voluptates soluta voluptatibus
+      natus quia sit accusamus, cum provident autem.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+      ad quos inventore aperiam voluptates soluta voluptatibus
+      natus quia sit accusamus, cum provident autem.
+    </p>
+  </div>
+);
+
+export default Home;
