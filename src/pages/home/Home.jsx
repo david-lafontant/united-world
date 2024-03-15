672c72d59027ae9b8f8accc1e5cc2b4fcba23dbb
David Lafontant <lafontant.david@gmail.com>
2024-03-15 11:29:49 -0400

    Add dummy coontent to Home view

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,83 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
-const Home = () => (
-  <div>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
-      ad quos inventore aperiam voluptates soluta voluptatibus
-      natus quia sit accusamus, cum provident autem.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
-      ad quos inventore aperiam voluptates soluta voluptatibus
-      natus quia sit accusamus, cum provident autem.
-    </p>
-  </div>
-);
+function Home() {
+  return (
+    <Container className="p-4 mt-4">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Excepturi sapiente necessitatibus quaerat ut quisquam, iste sint
+        ad quos inventore aperiam voluptates soluta voluptatibus
+        natus quia sit accusamus, cum provident autem.
+      </p>
+    </Container>
+  );
+}
 
 export default Home;
